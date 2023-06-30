70a85001c8ec0f232e41c7aefb326ffb431896eb
Reid Gendron <64567757+reidgendron@users.noreply.github.com>
2023-06-30 16:02:53 -0700

    Stylesheet changes

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {
    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-      document.getElementById("header").style.boxShadow = "1px 3px 8px 1px rgba(0,0,0,0.1)";
+      document.getElementById("header").style.boxShadow = "0 .125rem .25rem 0 rgba(0,0,0,0.1)";
    } else {
-      document.getElementById("header").style.boxShadow = "0px 0px 0px 0px rgba(0,0,0,0)";
+      document.getElementById("header").style.boxShadow = "0 0 0 0 rgba(0,0,0,0)";
    };
 };
