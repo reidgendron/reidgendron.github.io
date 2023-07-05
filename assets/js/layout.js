08d4946e4468898b0e65a1a8bea80b1eb1ebdb50
Reid Gendron <64567757+reidgendron@users.noreply.github.com>
2023-07-04 19:23:02 -0700

    Major changes to stylesheet

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,6 +1,6 @@
 window.onscroll = function() {
    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-      document.getElementById("header").style.boxShadow = "0 .125rem .25rem 0 rgba(0,0,0,0.1)";
+      document.getElementById("header").style.boxShadow = "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px";
    } else {
       document.getElementById("header").style.boxShadow = "0 0 0 0 rgba(0,0,0,0)";
    };
